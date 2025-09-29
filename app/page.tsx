773af41045628acee9ecf3ea63e7aae3840fdab7
Shuveloper <shuveloper0308@gmail.com>
2025-09-29 15:18:24 +0900

    feat layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+import { NavBar } from "./components/navBar/NavBar";
+import { DashboardContent } from "./components/DashboardContent";
+
 export default function Home() {
-  return <div className="font-sans">Hello</div>;
+  return (
+    <div className="font-sans min-h-screen bg-gray-50">
+      <DashboardContent />
+    </div>
+  );
 }
